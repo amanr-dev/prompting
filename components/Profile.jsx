3e35f29934c63731f7ae4ff9b05dff339d0b25f4
Aman <dv451197@gmail.com>
2023-11-26 21:34:02 +0530

    updates

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -13,15 +13,17 @@ const Profile = ({
     <section className="w-full">
       <div className="flex items-center justify-between">
         <h1 className="head_text text-left">
-          <span className="blue_gradient capitalize">{name}'s Profile</span>
+          <span className="blue_gradient capitalize">
+            {name}'s <span className="orange_gradient text-5xl">Profile</span>
+          </span>
         </h1>
         <div className="w-[250px] h-[250px] shadow-md flex flex-col items-center justify-center ">
           <img src={img} className="rounded-full w-[150px]" alt="User-image" />
           <h4 className="capitalize blue_gradient">{name}</h4>
-          <h5 className="text-slate-500">{about}</h5>
+          <h5 className="text-slate-500 capitalize">{about}</h5>
         </div>
       </div>
-      <p className="desc text-left leading-loose">{desc}</p>
+      <p className="desc text-left capitalize">{desc}</p>
 
       <div className="mt-10 prompt_layout">
         {data.map((post) => (
